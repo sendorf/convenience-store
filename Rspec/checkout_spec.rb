a0e090b24bad37a9be2a1bd43fe33e834afc537e
Sergio Díaz <sendorf@gmail.com>
2016-11-22 23:05:30 +0100

    Update task

--- Rspec/checkout_spec.rb
+++ Rspec/checkout_spec.rb
@@ -3,10 +3,7 @@ require_relative '../checkout.rb'
 
 describe Checkout, "#Basket 1" do
   it "The result should be 22.45." do
-                      # As long as the second green tea is free it is removed from the shopping basket in order to calculate the total
-    pricing_rules = {"rule1" => {:cond => '(@items.count("GR1")/2) >= 1', :result => '(@items.count("GR1")/2).times do @items.delete_at(@items.index("GR1")) end'},
-                     # If there are 3 or more strawberries the price for them changes
-                     "rule2" => {:cond => '@items.count("SR1") >= 3', :result => '@articles["SR1"] = {:name => "Strawberries", :price => 4.50}'}}
+    pricing_rules = [{:rule=>'buy_one_get_one_free', :params => ["GR1"]}, {:rule=>'bulk_discount', :params => ["SR1", 4.50]}]
     co = Checkout.new(pricing_rules)
     co.scan("GR1")
     co.scan("SR1")
@@ -19,10 +16,7 @@ end
 
 describe Checkout, "#Basket 2" do
   it "The result should be 3.11." do
-                      # As long as the second green tea is free it is removed from the shopping basket in order to calculate the total
-    pricing_rules = {"rule1" => {:cond => '(@items.count("GR1")/2) >= 1', :result => '(@items.count("GR1")/2).times do @items.delete_at(@items.index("GR1")) end'},
-                     # If there are 3 or more strawberries the price for them changes
-                     "rule2" => {:cond => '@items.count("SR1") >= 3', :result => '@articles["SR1"] = {:name => "Strawberries", :price => 4.50}'}}
+    pricing_rules = [{:rule=>'buy_one_get_one_free', :params => ["GR1"]}, {:rule=>'bulk_discount', :params => ["SR1", 4.50]}]
     co = Checkout.new(pricing_rules)
     co.scan("GR1")
     co.scan("GR1")
@@ -32,10 +26,7 @@ end
 
 describe Checkout, "#Basket 3" do
   it "The result should be 16.61." do
-                      # As long as the second green tea is free it is removed from the shopping basket in order to calculate the total
-    pricing_rules = {"rule1" => {:cond => '(@items.count("GR1")/2) >= 1', :result => '(@items.count("GR1")/2).times do @items.delete_at(@items.index("GR1")) end'},
-                     # If there are 3 or more strawberries the price for them changes
-                     "rule2" => {:cond => '@items.count("SR1") >= 3', :result => '@articles["SR1"] = {:name => "Strawberries", :price => 4.50}'}}
+    pricing_rules = [{:rule=>'buy_one_get_one_free', :params => ["GR1"]}, {:rule=>'bulk_discount', :params => ["SR1", 4.50]}]
     co = Checkout.new(pricing_rules)
     co.scan("SR1")
     co.scan("SR1")
--- checkout.rb
+++ checkout.rb
@@ -1,40 +1,37 @@
 # checkout.rb
 class Checkout
 
-  attr_reader :promotional_rules, :articles, :items
+  GR1 = {:name => "Green tea", :price => 3.11}
+  SR1 = {:name => "Strawberries", :price => 5.00}
+  CF1 = {:name => "Coffee", :price => 11.23}
 
-  def initialize(promotional_rules = {})
-    @promotional_rules = promotional_rules                                   # Initializes the promotional rules that may be applied
-    @articles = {"GR1" => {:name => "Green tea", :price => 3.11},           # for the checkout
-                 "SR1" => {:name => "Strawberries", :price => 5.00},  
-                 "CF1" => {:name => "Coffee", :price => 11.23}}            # Initializes the articles in the shop
+  def initialize(pricing_rules = {})
+    @pricing_rules = pricing_rules                                   # Initializes the promotional rules that may be applied
     @items = []
   end
 
   def scan(item)
-    @items << item                                                          # Adds the key of every item to an array of keys
+    @items << Checkout.const_get(item).clone                                                         # Adds the key of every item to an array of keys
   end                                                                       # of scanned items
 
+  def buy_one_get_one_free(product)
+    product = Checkout.const_get product
+    (@items.select{|x| x[:name] == product[:name]}.size/2).times do
+      free_item = @items.select{|x| x[:name] == product[:name] && x[:price] != 0}.last
+      free_item[:price] = 0
+    end
+  end
+
+  def bulk_discount(product, discount_price)
+    product = Checkout.const_get product
+    discountable_items = @items.select{|x| x[:name] == product[:name]}
+    if discountable_items.size >= 3
+      discountable_items.each{|x| x[:price] = discount_price}
+    end
+  end
+
   def total()
-    total = 0
-    if(promotional_rules.size != 0)                                         # Checks if any of the rules is applicable before
-      promotional_rules.each do |key, rule|                                 # processing the total
-        if(eval(rule[:cond]))
-          eval(rule[:result])
-        end
-      end
-    end
-    @items.each do |item|                                                   # Calculates the total from the scanned items
-      article = @articles.values_at(item).first
-      total = article[:price] + total
-    end
-    if(promotional_rules.size != 0)                                         # Checks if any of the rules is applicable after
-      promotional_rules.each do |key, rule|                                 # processing the total
-        if(eval(rule[:cond]))
-          eval(rule[:result])
-        end
-      end
-    end
-    total.round(2)
+    @pricing_rules.each{|x| send(x[:rule], *x[:params])}
+    @items.map{|x| x[:price].to_f}.reduce(:+).round(2)
   end
 end
